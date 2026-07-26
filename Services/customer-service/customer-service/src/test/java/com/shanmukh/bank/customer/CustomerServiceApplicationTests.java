08714c859fe2343e0e573ba4b0dd4951bef70ed2
shanmu <itsshanmu@gmail.com>
2026-07-26 17:11:47 +0530

    Create Customer Service project

---
+++ Services/customer-service/customer-service/src/test/java/com/shanmukh/bank/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanmukh.bank.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
